f5a80922f991ad75d6137c9f3a7cbcbd476529d9
Delta Ink Design <izula97@gmail.com>
2019-01-20 20:01:00 +0100

    Add Console Class 2

---
+++ Console Class 2/Console Class 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Class_2
+{
+    public class Samochod
+    {
+        public string marka;
+        public string rejestracja;
+        public string kolor;
+        public Samochod(string mar, string rej, string kol)
+        {
+            marka = mar;
+            rejestracja = rej;
+            kolor = kol;
+        }
+        public void Info()
+        {
+            Console.WriteLine("Marka: " + marka + " Rejestracja: " + rejestracja + " Kolor: " + kolor);
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Samochod p1 = new Samochod("Audi", "PLE 997", "czarny");
+            p1.Info();
+            Console.ReadKey();
+        }
+    }
+}
