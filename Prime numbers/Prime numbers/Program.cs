abc14a332359327d719255e81393f05e381032e2
Delta Ink Design <izula97@gmail.com>
2019-01-20 18:32:36 +0100

    Add Prime numbers

---
+++ Prime numbers/Prime numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string wejscie = Console.ReadLine();
+            try
+            {
+                int j = int.Parse(wejscie);
+                for (int i = 2; i <= j; i++)
+                {
+                    if (j % i != 0)
+                    {
+                        Console.WriteLine("Liczba jest pierwsza");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Liczba nie jest pierwsza!");
+                        
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Podano zły typ");
+            }
+        }
+    }
+}
