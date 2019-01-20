82cf516868727b26a10f3b9acd8aeb12da14f9cc
Delta Ink Design <izula97@gmail.com>
2019-01-20 19:44:28 +0100

    Rectangle Struct

---
+++ Rectangle Struct/Rectangle Struct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Struct
+{
+    class Program
+    {
+        struct Rectangle
+        {
+            public int width;
+            public int height;
+            public string Show()
+            {
+
+                return "[" + width + ";" + height + "]";
+            }
+        }
+        static void Main()
+        {
+            Rectangle r1 = new Rectangle();
+            r1.width = 10;
+            r1.height = 4;
+            Console.WriteLine(r1.Show());
+
+        }
+
+    }
+}
