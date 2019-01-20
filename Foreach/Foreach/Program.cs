aa9483b86d2b4e78ea361fcd4cd97cb9457f0d49
Delta Ink Design <izula97@gmail.com>
2019-01-20 19:06:40 +0100

    Add Foreach

---
+++ Foreach/Foreach/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Podaj rozmiar tablicy");
+            int rozmiar = int.Parse(Console.ReadLine());
+            int[] tablica = new int[rozmiar];
+            Random rand = new Random();
+            for (int i = 0; i < rozmiar; i++)
+                tablica[i] = rand.Next(1, 10);
+            Array.Sort(tablica);
+            foreach (int element in tablica)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
