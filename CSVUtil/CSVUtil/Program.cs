425b5c3c7d4d7bbf2028cda0a13e8282c730c8ff
Delta Ink Design <izula97@gmail.com>
2019-01-20 19:27:42 +0100

    Add

---
+++ CSVUtil/CSVUtil/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVUtil
+{
+    public class CSVUtil
+    {
+        static string[] tablica;
+        static int[] tablicaliczb;
+
+        public static int znajdzNajw(string tekst)
+        {
+            try
+            {
+
+                podziel(tekst);
+                Array.Sort(tablicaliczb);
+                return tablicaliczb[tablica.Length - 1];
+            }
+            catch (Exception e)
+            {
+                throw new FormatException();
+                return 0;
+            }
+        }
+
+        public static int znajdzNajm(string tekst)
+        {
+            try
+            {
+                podziel(tekst);
+                Array.Sort(tablicaliczb);
+                return tablicaliczb[0];
+
+            }
+            catch (Exception e)
+            {
+                throw new FormatException();
+                return 0;
+            }
+        }
+
+        public static string sortuj(string tekst)
+        {
+            try
+            {
+                podziel(tekst);
+                Array.Sort(tablicaliczb);
+                string tablicazwrot = "";
+                int i = 0;
+                while (i != tablicaliczb.Length)
+                {
+                    tablicazwrot += tablicaliczb[i] + ",";
+                    i++;
+                }
+                return tablicazwrot;
+
+            }
+            catch (Exception e)
+            {
+                throw new FormatException();
+
+                return "null";
+            }
+        }
+
+        public static void podziel(string tekst)
+        {
+            tablica = tekst.Split(',');
+            tablicaliczb = new int[tablica.Length];
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                tablicaliczb[i] = int.Parse(tablica[i]);
+            }
+
+        }
+
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+
+
+            for (int i = 0; i < CSVUtil.sortuj(input).Length; i++)
+            {
+                Console.Write(CSVUtil.sortuj(input)[i]);
+            }
+
+
+            Console.WriteLine("\n" + CSVUtil.znajdzNajm(input));
+            Console.WriteLine("\n" + CSVUtil.znajdzNajw(input));
+
+        }
+    }
+}
---
+++ String Builder/String Builder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Builder
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            StringBuilder mSB = new StringBuilder("", 100);
+            for (int i = 0; i < 16; i++)
+            {
+                mSB.Append("na ");
+            }
+            mSB.Append("Batman!!!");
+            Console.WriteLine(mSB.ToString());
+            Console.WriteLine("Batmana znajdziesz w: " + mSB.ToString().IndexOf("Batman"));
+            mSB.Insert(mSB.ToString().IndexOf("Batman"), "...");
+            Console.WriteLine(mSB.ToString());
+        }
+    }
+}
